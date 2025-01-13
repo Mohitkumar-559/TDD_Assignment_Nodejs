e0ad88e454bb4f2b231fa0ba5b194a3660e241d3
mohit kumar <mohit.k@kodehash.com>
2025-01-13 16:55:48 +0530

    added the case for should return the number itself for a signle number

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -24,39 +24,7 @@ describe("StringCalculator", () => {
     expect(calculator.add("5")).to.equal(5);
   });
 
-  it("should return the sum of two numbers", () => {
-    expect(calculator.add("1,2")).to.equal(3);
-    expect(calculator.add("3,5")).to.equal(8);
-  });
-
-  it("should handle any amount of numbers", () => {
-    expect(calculator.add("1,2,3,4")).to.equal(10);
-    expect(calculator.add("10,20,30,40,50")).to.equal(150);
-  });
-
-  it("should handle newlines as delimiters", () => {
-    expect(calculator.add("1\n2,3")).to.equal(6);
-    expect(calculator.add("4\n5\n6")).to.equal(15);
-  });
-
-  it("should support custom delimiters", () => {
-    expect(calculator.add("//;\n1;2")).to.equal(3);
-    expect(calculator.add("//|\n1|2|3")).to.equal(6);
-    expect(calculator.add("//***\n1***2***3")).to.equal(6);
-  });
-  
-  it("should handle special characters in custom delimiters", () => {
-    expect(calculator.add("//.\n1.2.3")).to.equal(6);   // Dot as a delimiter
-    expect(calculator.add("//$\n1$2$3")).to.equal(6);   // Dollar sign as a delimiter
-    expect(calculator.add("//***\n1***2***3")).to.equal(6); // Multi-character delimiter
-  });
   
 
-  it("should throw an exception for negative numbers", () => {
-    expect(() => calculator.add("-1,2,-3")).to.throw("Negative numbers not allowed: -1, -3");
-  });
-
-  it("should throw an exception listing all negative numbers", () => {
-    expect(() => calculator.add("5,-1,-2,3")).to.throw("Negative numbers not allowed: -1, -2");
-  });
+  
 });
