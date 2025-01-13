c607e64533f2380606e069d7d3c52cf6f6a38a11
mohit kumar <mohit.k@kodehash.com>
2025-01-13 16:58:53 +0530

    added test case for any new line character

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -27,7 +27,18 @@ describe("StringCalculator", () => {
     expect(calculator.add("1,2")).to.equal(3);
     expect(calculator.add("3,5")).to.equal(8);
   });
+  it("should handle any amount of numbers", () => {
+    expect(calculator.add("1,2,3,4")).to.equal(10);
+    expect(calculator.add("10,20,30,40,50")).to.equal(150);
+  });
 
+  it("should handle newlines as delimiters", () => {
+    expect(calculator.add("1\n2,3")).to.equal(6);
+    expect(calculator.add("4\n5\n6")).to.equal(15);
+  });
+
+  
+  
   
 
   
